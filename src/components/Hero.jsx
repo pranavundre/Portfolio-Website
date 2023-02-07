7d27428000643694f5906b74c64f4a5702755417
pranavundre <undrepranav@gmail.com>
2023-02-07 22:13:21 +0530

    Navbar & Home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+
+  const social_media = [{
+    name: "logo-github", logo: `<ion-icon name="logo-github"></ion-icon>`, link : "https://github.com/pranavundre"
+  }, {
+    name: "code-outline", logo: `<ion-icon name="code-outline"></ion-icon>`, link : "https://leetcode.com/pranavundre/"
+  }, {
+    name : "logo-linkedin", logo: `<ion-icon name="logo-linkedin"></ion-icon>`, link : "https://www.linkedin.com/in/pranav-undre-920961177/"
+  },{
+    name : "logo-instagram", logo: `<ion-icon name="logo-instagram"></ion-icon>`, link : "https://www.instagram.com/pranavuuu.dmg/"
+  },{
+    name : "logo-twitter", logo: `<ion-icon name="logo-twitter"></ion-icon>`, link : "https://twitter.com/pranav_undre"
+  }];
+
+  return <section
+  id = "home" 
+  className="min-h-screen flex py-10 md:flex-row flex-col items-center">
+    <div className="flex-auto flex items-center justify-center h-full">
+      {/* <img src={hero} alt="" className="md:w-11/12 h-full object-cover"/> */}
+    </div>
+    <div className="flex-auto">
+      <div className="md:text-left text-center">
+        <h1 className="md:text-5xl md:text-left text-2xl text-center md:leading-normal leading-10 text-white font-bold">
+          <span className="text-blue-400 md:text-6xl text-5xl">Hello! <br /></span>
+          My name is <span>Pranav Undre</span>
+        </h1>
+        <h4 className="md:text-2xl md:leading-normal leading-5 mt-4 text-base text-gray-400 font-light">Web Developer | Competitive Programmer</h4>
+        
+        <button className="bg-blue-500 md:mx-0 mx:auto px-6 py-3 rounded-full mt-8 md:text-xl text-base mx-auto flex items-center">Say Hi 👋</button>
+        
+        <div className="flex gap-6 md:gap-8 mt-8 text-2xl md:text-4xl text-gray-600 items-center md:justify-start justify-center">
+            {
+              social_media?.map((site) => (
+                <div key = {site} className = "hover:text-blue-400">
+                  <a href={site.link}>
+                  <ion-icon name={site.name}></ion-icon>
+                  </a>
+                </div>
+              ))
+            }
+        </div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [sticky, setSticky] = useState(false);
+  const [open, setOpen] = useState (false);
+
+  const menuLinks = [{
+    name: "HOME", link : "#home"
+  }, {
+    name: "ABOUT", link : "#about"
+  }, {
+    name : "SKILLS", link : "#skills"
+  },{
+    name : "PROJECTS", link : "#projects"
+  },{
+    name : "CONTACT", link : "#contact"
+  }];
+
+  useEffect(()=> {
+    window.addEventListener("scroll", ()=> {
+      const nav = document.querySelector('nav')
+      window.scrollY > 0 ? setSticky(true) : setSticky(false);
+    })
+  })
+
+  return <nav className={`fixed w-full left-0 top-0 z-[999] ${ sticky ? "bg-white/60 text-gray-900" : "text-white"}`}>
+    <div className="flex items-center justify-between">
+      <div className="md:mx-8 my-auto pl-4">
+        <h4 className="text-4xl uppercase font-bold tracking-wider">
+          Prana<span className="text-blue-600">v</span>
+        </h4>
+      </div>
+      <div className={`${sticky ? "bg-white/0" : "bg-white"} text-gray-900 md:block hidden px-10 py-4 font-medium rounded-bl-3xl `}>
+        <ul className="flex items-center gap-1 py-2 text-lg">
+          {
+            menuLinks?.map((menu, i) => (
+              <li className="px-6 text-blue-900 hover:text-blue-600" key = {i}>
+                <a href={menu?.link}>
+                  {menu?.name}
+                </a>
+              </li>
+            ))
+          }
+        </ul>          
+      </div>
+
+
+      <div 
+      onClick={()=>setOpen(!open)}
+      className = {`z-[999] ${open ? "text-gray-900" : "text-gray-100"} text-4xl md:hidden mt-6 mb-4 mr-4`}>
+        <ion-icon name = "menu"></ion-icon>
+      </div>
+
+      <div className={`md:hidden text-gray-800 absolute w-2/3 h-screen px-4 py-2 font-medium bg-white top-0 duration-500 ${
+        open ? "right-0" : "right-[-100%]"
+        }`}>
+        <ul className="flex flex-col justify-center h-full gap-4 py-2 text-lg ">
+          {
+            menuLinks?.map((menu, i) =>  (
+              <li className="px-6 hover:text-blue-400" key = {i}>
+                <a href = {menu?.link}> {menu?.name}</a>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
+  </nav>;
 };
 
 export default Navbar;
